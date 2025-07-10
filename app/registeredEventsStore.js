3ab467d16e211c946a59585754753013519832cc
ZesterJ <yjianwei.05@gmail.com>
2025-07-10 22:17:52 +0800

    Update registeredEventsStore.js

--- app/registeredEventsStore.js
+++ app/registeredEventsStore.js
@@ -2,28 +2,25 @@ const store = {
   registeredEventsGlobal: [
     {
       id: '1',
-      date: '15 March 2025',
-      title: 'Science Centre',
-      details: 'Saturday 10AM - 1PM @ Science Centre\nPick up @ Pek Kio CC',
+      date: '2025年3月15日',
+      title: '科学中心',
+      details: '星期六 上午10点 - 下午1点 @ 科学中心\n集合地点：白桥民众俱乐部',
       paid: false,
     },
-    // DO NOT include Durian Fiesta here if you want to test adding it!
+
   ],
 };
 
-
-
 export function addRegisteredEvent(event) {
-  console.log('Trying to add event:', event);
+  console.log('尝试添加活动:', event);
   if (!store.registeredEventsGlobal.some(e => String(e.id) === String(event.id))) {
     store.registeredEventsGlobal.push(event);
-    console.log('Event added:', event);
+    console.log('已添加活动:', event);
   } else {
-    console.log('Event already exists:', event.id);
+    console.log('活动已存在:', event.id);
   }
-  console.log('Current store:', store.registeredEventsGlobal);
+  console.log('当前活动列表:', store.registeredEventsGlobal);
 }
-
 
 export function removeRegisteredEvent(eventId) {
   store.registeredEventsGlobal = store.registeredEventsGlobal.filter(
@@ -31,5 +28,5 @@ export function removeRegisteredEvent(eventId) {
   );
 }
 
-
 export default store;
+
